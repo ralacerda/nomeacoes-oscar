a6930b210de930ddd9fe931df10d2eb7f8df02a4
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-01-18 18:12:17 -0300

    feat: 2021 movies and new initial page

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,5 +1,11 @@
 import awards from "~/data/awards.json";
 import oscar2022 from "~/data/resultOscar2022";
+import oscar2021 from "~/data/resultOscar2021";
+
+const oscarByYear = {
+  2022: oscar2022,
+  2021: oscar2021,
+};
 
 export function getAwardName(award) {
   return awards[award];
@@ -9,14 +15,14 @@ export function getAwardList() {
   return Object.keys(awards);
 }
 
-export function getMovies() {
-  return oscar2022;
+export function getMovies(awardYear) {
+  return oscarByYear[awardYear];
 }
 
-export function getMovieList() {
-  return Object.keys(oscar2022);
+export function getMovieList(awardYear) {
+  return Object.keys(oscarByYear[awardYear]);
 }
 
-export function getMovieBySlug(slug) {
-  return oscar2022[slug];
+export function getMovieBySlug(slug, awardYear) {
+  return oscarByYear[awardYear][slug];
 }
