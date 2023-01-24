00de72e3ea9e2663027276580601a0d6f541aa52
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-01-24 11:48:09 -0300

    feat: 2023 nominees

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,10 +1,12 @@
 import awards from "~/data/awards.json";
 import oscar2022 from "~/data/resultOscar2022";
 import oscar2021 from "~/data/resultOscar2021";
+import oscar2023 from "~/data/resultOscar2023";
 
 const oscarByYear = {
   2022: oscar2022,
   2021: oscar2021,
+  2023: oscar2023,
 };
 
 export function getAwardName(award) {
